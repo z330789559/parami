2d0058fef58ffa03b2faeb62533f6cf3fd6f2966
andelf <andelf@gmail.com>
2021-02-21 20:07:29 +0800

    *: fmt

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -85,7 +85,8 @@ pub fn run() -> Result<()> {
                 match config.role {
                     Role::Light => service::new_light(config),
                     _ => service::new_full(config),
-                }.map_err(sc_cli::Error::Service)
+                }
+                .map_err(sc_cli::Error::Service)
             })
         }
         Some(Subcommand::Inspect(cmd)) => {
--- cli/src/service.rs
+++ cli/src/service.rs
@@ -491,7 +491,5 @@ pub fn new_light_base(
 
 /// Builds a new service for a light client.
 pub fn new_light(config: Configuration) -> Result<TaskManager, ServiceError> {
-    new_light_base(config).map(|(task_manager, _, _, _, _, _)| {
-		task_manager
-	})
+    new_light_base(config).map(|(task_manager, _, _, _, _, _)| task_manager)
 }
